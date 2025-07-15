43bab5b5fa9cf455cf0745aa51a7898f7ed6f074
Arys <charles.yaiche@epita.fr>
2025-07-15 14:45:02 +0200

    add mnist loader

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from loguru import logger
 
 load_dotenv()
 
-PROJ_ROOT = Path(__file__).resolve().parents[1]
+PROJ_ROOT = Path(__file__).resolve().parents[0]
 logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
 
 DATA_DIR = PROJ_ROOT / "data"
