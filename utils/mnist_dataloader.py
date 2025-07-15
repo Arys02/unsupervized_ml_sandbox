3a12b28438156c77622c149994c11079fecaf7ea
Arys <charles.yaiche@epita.fr>
2025-07-15 14:45:02 +0200

    add mnist loader

---
+++ utils/mnist_dataloader.py
@@ -0,0 +1,8 @@
+from keras.datasets import mnist
+from config import PROCESSED_DATA_DIR
+import numpy as np
+
+mnist.load_data()
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x = x_train.reshape(60000, 784).astype(np.float32) / 255.0
+np.save(PROCESSED_DATA_DIR / "mnist_flattened.npy", x_train)
