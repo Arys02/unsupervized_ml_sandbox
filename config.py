379295153307ce0eeb6afce45c7b34ca33a00bef
arys <charles.yaiche.lf@gmail.com>
2025-07-17 13:39:16 +0200

    add card_explorer file and notebook for presentation

--- config.py
+++ config.py
@@ -10,6 +10,8 @@ logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
 
 DATA_DIR = PROJ_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
+RAW_CARD_DIR = RAW_DATA_DIR / "archive"
+
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
 GENERATED_DATA_DIR = DATA_DIR / "generated"
 
